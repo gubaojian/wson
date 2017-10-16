17f2e0e09d90e22f973d72c823cfa68bdb5f2771
jianbai.gbj <jianbai.gbj@alibaba-inc.com>
2017-10-16 15:32:57 +0800

    supported

---
+++ java/src/test/java/com/furture/wson/feature/FastJSONTest.java
@@ -0,0 +1,17 @@
+package com.furture.wson.feature;
+
+import com.alibaba.fastjson.JSON;
+
+/**
+ * Created by 剑白(jianbai.gbj) on 2017/10/16.
+ */
+public class FastJSONTest {
+
+    public static void main(String[] args){
+
+
+        System.out.println( JSON.parse("true"));
+
+
+    }
+}
